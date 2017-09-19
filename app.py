50366ce9bf240ac65f871273ac7e81d920dfb4de
yukinoba <ckmagician@gmail.com>
2017-09-19 16:19:45 +0800

    requires string as left operand, not bytes

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ content = tn.read_very_eager().decode('uao_decode');
 # print(content);
 
 if "請輸入代號" in content:
-    tn.write('guest^m');
+    tn.write("guest^m");
     time.sleep(3);
     content = tn.read_very_eager().decode('uao_decode');
     
