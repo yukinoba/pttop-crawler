1a7e239d597f86daf2eeef866f887faf3e3256e3
yukinoba <ckmagician@gmail.com>
2017-09-20 11:18:54 +0800

    change ^m back to \r\n

--- app.py
+++ app.py
@@ -13,12 +13,12 @@ content = tn.read_very_eager().decode('uao_decode');
 # print(content);
 
 if "請輸入代號" in content:
-    tn.write("SeptemberCat".encode('cp950') + b"^m");
+    tn.write("SeptemberCat".encode('cp950') + b"\r\n");
     time.sleep(3);
     content = tn.read_very_eager().decode('uao_decode');
     
     if "請輸入您的密碼" in content:
-        tn.write("meow".encode('cp950') + b"^m");
+        tn.write("meow".encode('cp950') + b"\r\n");
         time.sleep(3);
         content = tn.read_very_eager().decode('uao_decode');
 
