0c70e6cdc18c2b06602a20f8820a9e4c52643ba0
yukinoba <ckmagician@gmail.com>
2017-09-20 11:56:34 +0800

    console debug log

--- app.py
+++ app.py
@@ -13,13 +13,27 @@ content = tn.read_very_eager().decode('uao_decode');
 # print(content);
 
 if "請輸入代號" in content:
+    print(">>> 輸入帳號" + b"\r\n");
     tn.write("SeptemberCat".encode('cp950') + b"\r\n");
     time.sleep(3);
     content = tn.read_very_eager().decode('uao_decode');
     
     if "請輸入您的密碼" in content:
+        print(">>> 輸入密碼" + b"\r\n");
         tn.write("meow".encode('cp950') + b"\r\n");
         time.sleep(3);
         content = tn.read_very_eager().decode('uao_decode');
+        
+        if "您想刪除其他重複登入的連線嗎" in content:
+            print(">>> 刪除重複登入" + b"\r\n");
+            tn.write("Y".encode('cp950') + b"\r\n");
+            time.sleep(3);
+            content = tn.read_very_eager().decode('uao_decode');
+        
+        if "請按任意鍵繼續" in content:
+            print(">>> 登入成功" + b"\r\n");
+            tn.write(" ".encode('cp950') + b"\r\n");
+            time.sleep(3);
+            content = tn.read_very_eager().decode('uao_decode');
 
 print(content);
