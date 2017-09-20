28ffab764f3ca3554143934a766121f10eba9104
yukinoba <ckmagician@gmail.com>
2017-09-20 10:04:13 +0800

    encode('uao_decode')

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ content = tn.read_very_eager().decode('uao_decode');
 # print(content);
 
 if "請輸入代號" in content:
-    tn.write("guest\r\n");
+    tn.write("guest".encode('uao_decode') + b"\r\n");
     time.sleep(3);
     content = tn.read_very_eager().decode('uao_decode');
     
