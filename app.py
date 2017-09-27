57452d07c0629ed64157200d836acbdcb7b1408e
yukinoba <ckmagician@gmail.com>
2017-09-27 13:58:34 +0800

    1. get control code content 2. add cap limit

--- app.py
+++ app.py
@@ -99,6 +99,7 @@ for mo in matches:
     cap = index + 10;
     if cap > len(content):
         cap = len(content) - 1;
+    print(">>> 發現控制碼");
     print(content[start:cap]);
     count = count + 1;
 print(">>> 控制碼數量：" + str(count));
