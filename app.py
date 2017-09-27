655a7dd34207e4c36001e3104ddb50ab577b9cae
nicholas_chien <ckmagician@gmail.com>
2017-09-27 22:49:29 +0800

    get last and new push

--- app.py
+++ app.py
@@ -13,34 +13,50 @@
         # for title in res.select('div.title'):
             # print(title.select('a')[0].text);
 
+import sys
+import time
 import http.client
 from bs4 import BeautifulSoup
 
+last_newpush_list = [];
 conn = http.client.HTTPSConnection("www.ptt.cc");
-conn.request("GET", "/bbs/ONE_PIECE/index.html");
-response_list = conn.getresponse();
-content_list = response_list.read().decode(response_list.headers.get_content_charset('utf-8'));
 
-soup_list = BeautifulSoup(content_list, 'html.parser');
-for title in soup_list.select('div.title'):
-    print(">>> 讀取標題：" + title.select('a')[0].text);
-    if "檢舉區" in title.select('a')[0].text:
-        print(">>> 檢舉區連結：" + title.select('a')[0]['href']);
-        prosec_href = title.select('a')[0]['href'];
-        
-        conn.request("GET", prosec_href);
-        response_prosec = conn.getresponse();
-        content_prosec = response_prosec.read().decode(response_prosec.headers.get_content_charset('utf-8'));
-        
-        soup_prosec = BeautifulSoup(content_prosec, 'html.parser');
-        for push in soup_prosec.select('div.push'):
-            print(">>> 讀取推文：" + push.text);
-        for lastpush in soup_prosec.select('div.push:last-of-type'):
-            print(">>> 最後一則推文為：" + lastpush.text);
-        for lastedit in soup_prosec.select('span.f2'):
-            if "編輯" in lastedit.text:
-                for newpush in lastedit.find_next_siblings('div', 'push'):
-                    print(">>> 有新推文：" + newpush.text);
+while True:
+    conn.request("GET", "/bbs/ONE_PIECE/index.html");
+    response_list = conn.getresponse();
+    content_list = response_list.read().decode(response_list.headers.get_content_charset('utf-8'));
+
+    soup_list = BeautifulSoup(content_list, 'html.parser');
+    for title in soup_list.select('div.title'):
+        print(">>> 讀取標題：" + title.select('a')[0].text);
+        if "檢舉區" in title.select('a')[0].text:
+            print(">>> 檢舉區連結：" + title.select('a')[0]['href']);
+            prosec_href = title.select('a')[0]['href'];
+            
+            conn.request("GET", prosec_href);
+            response_prosec = conn.getresponse();
+            content_prosec = response_prosec.read().decode(response_prosec.headers.get_content_charset('utf-8'));
+            
+            soup_prosec = BeautifulSoup(content_prosec, 'html.parser');
+            for lastpush in soup_prosec.select('div.push'):
+                pass
+            print(">>> 最後推文：" + lastpush.text);
+            
+            newcoming = False;
+            soup_prosec = BeautifulSoup(content_prosec, 'html.parser');
+            for lastedit in soup_prosec.select('span.f2'):
+                if "編輯" in lastedit.text:
+                    for newpush in lastedit.find_next_siblings('div', 'push'):
+                        if newpush.text in last_newpush_list:
+                            print(">>> 尚未處理：" + newpush.text);
+                        else:
+                            print(">>> 有新推文：" + newpush.text);
+                            if not newcoming:
+                                del last_newpush_list[:];
+                                newcoming = True;
+                            last_newpush_list.append(newpush.text);
+                        
+    time.sleep(60 * 1);
 
 # import telnetlib
 # import uao_decode
